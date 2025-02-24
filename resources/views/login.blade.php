6ea3d05368f7d232a3e24b7d853732a30fdde992
pulkitamga <pulkit.amga@gmail.com>
2025-02-24 17:07:07 +0530

    abxscsc

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,7 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Admin Dashboard</title>
+    <title>Admin Dashboard abccdvjknjnkj</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="" />
 
