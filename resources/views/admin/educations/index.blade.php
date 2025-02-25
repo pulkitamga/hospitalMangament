4039174a85479a359981a2517ab4c1e5a61ea75b
pulkitamga <pulkit.amga@gmail.com>
2025-02-25 18:49:02 +0530

    abc

--- resources/views/admin/educations/index.blade.php
+++ resources/views/admin/educations/index.blade.php
@@ -55,7 +55,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Education</h5>
+                <h5 class="modal-title">Add Education</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
