c86ae7136e6eb616337240b4ef6027f29f799d99
pulkitamga <pulikt.amga@gmail.com>
2025-02-26 10:59:27 +0530

    push to main

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,17 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-    
-
-=======
->>>>>>> developer-P
     
     protected $fillable=['name','status'];
 
-=======
-    protected $fillable =['name','status'];
->>>>>>> developer-A
 }
--- routes/web.php
+++ routes/web.php
@@ -124,10 +124,6 @@ Route::controller(UserRoleController::class)->group(function(){
     Route::get('/role','index')->name('users.role');
     Route::post('/role','store')->name('role.store');
     Route::put('/role/{id}','update')->name('role.update');
-<<<<<<< HEAD
-
-=======
->>>>>>> developer-A
     Route::delete('/role/{id}','destroy')->name('roles.destroy');
 });
 
